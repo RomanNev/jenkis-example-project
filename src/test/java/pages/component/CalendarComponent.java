06acb32c53e203ec10155440a157aa8d3c6f8bba
RomanGolub <romannovoselovcl@gmail.com>
2022-03-22 18:15:05 +0300

    initial commit

---
+++ src/test/java/pages/component/CalendarComponent.java
@@ -0,0 +1,12 @@
+package pages.component;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class CalendarComponent {
+    public void setDate(String month, String year){
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $("[aria-label='Choose Friday, August 24th, 1900']").click();
+
+    }
+}
---
+++ src/test/java/tests/RegistrationFormPageTests.java
@@ -0,0 +1,65 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+public class RegistrationFormPageTests {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    //configuration variables
+    String firstName = "Volodya";
+    String lastName = "Golub";
+    String userEmail = "Golub@mail.guli";
+    String userNumber = "7111222334";
+    String gender = "Male";
+    String calendarMonth = "August";
+    String calendarYear = "1900";
+    String subjectsText = "English";
+    String hobbiesText = "Music";
+    String fileName = "golub.jpg";
+    String currentAddress = "Lavrushinsky Ln, 10, Moscow, 119017";
+    String stateName = "NCR";
+    String cityName = "Delhi";
+    String dateOfBirth = "24 August,1900";
+    String stateAndCityName = "NCR Delhi";
+    String fullName = "Roman Golub";
+
+
+    @Test
+    void successFillTest() {
+        // Act
+        registrationPage
+                .openPage()
+                .setFirstNameInput(firstName)
+                .setLastNameInput(lastName)
+                .setUserEmailInput(userEmail)
+                .setUserNumberInput(userNumber)
+                .selectGender(gender)
+                .setBirthDate(calendarMonth, calendarYear)
+                .setSubjectsInput(subjectsText)
+                .setHobby(hobbiesText)
+                .selectUploadPicture(fileName)
+                .setCurrentAddressInput(currentAddress)
+                .selectStateAndCity(stateName, cityName)
+                .submitForm();
+
+
+        // Assert
+        registrationPage
+                .checkForm(fullName)
+                .checkForm(userEmail)
+                .checkForm(gender)
+                .checkForm(userNumber)
+                .checkForm(dateOfBirth)
+                .checkForm(subjectsText)
+                .checkForm(fileName)
+                .checkForm(currentAddress)
+                .checkForm(stateAndCityName);
+
+
+    }
+
+}
