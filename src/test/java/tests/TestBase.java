7fcb56d7ae0ef5d4439cb7f7272ce3d4104db0b5
RomanGolub <romannovoselovcl@gmail.com>
2022-03-23 20:49:12 +0300

    refactor setup

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,19 +15,23 @@ public class TestBase {
     @BeforeAll
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-        String user = System.getProperty("user");
+
+        String login = System.getProperty("login");
         String password = System.getProperty("password");
-
-
         Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.remote = "https://" + login + ":" + password + "@" + System.getProperty("remoteBrowser");
+
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.remote = "https://" + user + ":" + password + "@" + System.getProperty("remoteBrowser");
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
+
+
     }
 
     @AfterEach
